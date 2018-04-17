246d31b6294ed791f14140b6ec9f85ed32fc3179
Pier <madstrum@gmail.com>
2018-04-17 20:14:56 +0800

    grnn takes very long

--- data_models.R
+++ data_models.R
@@ -2,7 +2,7 @@
 library(nnet)
 library(Metrics)
 
-setwd('C:/Users/pierl/OneDrive/Documents/R_Projects/KE5206NN')
+setwd('/Users/pierlim/R_Projects/KE5206NN')
 train_df <- read.csv('fields_removed.csv')
 
 # min max normalisation
@@ -11,19 +11,70 @@ min_share_val <- min(train_df$shares)
 maxs = apply(train_df, 2, max)
 mins = apply(train_df, 2 ,min)
 shares = as.numeric(train_df$shares)
-nn_df <- as.data.frame(scale(train_df, center = mins, scale = maxs - mins))
+train_scaled_df <- as.data.frame(scale(train_df, center = mins, scale = maxs - mins))
+
+# Neural Nets + BP
+nn_df <- train_scaled_df
 
 set.seed(42)
 cat("\nCreating and training a neural network . . \n")
 mynn <- nnet(shares ~ ., data=nn_df, linout=TRUE,
-             size=50,skip=TRUE, MaxNWts=10000, trace=FALSE, maxit=100)
+             size=30,skip=TRUE, MaxNWts=10000, trace=FALSE, maxit=100)
 pred <- predict(mynn)
 cm <- as.data.frame(cbind(nn_df$shares, pred))
 
-pred <-  pred * (max_share_val - min_share_val) + min_share_val
-rmse_result <- rmse(nn_df$shares, pred)
-shares <- (nn_df$shares * (max_share_val - min_share_val)) + min_share_val
+pred_shares <-  pred * (max_share_val - min_share_val) + min_share_val
+actual_shares <- (nn_df$shares * (max_share_val - min_share_val)) + min_share_val
+rmse_result <- rmse(actual_shares, pred_shares)
+print("Neural Network RMSE:")
+print(rmse_result)
 plot(shares, pred, col='blue', pch=16, ylab="predicted shares", xlab="real shares")
+
+# RBF Model
+library(RSNNS)
+nn_df <- train_scaled_df
+set.seed(42)
+cat("\nCreating and training a RBF network . . \n")
+rbf_model <- rbf(nn_df, nn_df$shares, size=5, linOut=TRUE)
+rbf_predict <- fitted(rbf_model)
+
+pred_shares <-  rbf_predict * (max_share_val - min_share_val) + min_share_val
+actual_shares <- (nn_df$shares * (max_share_val - min_share_val)) + min_share_val
+rmse_result <- rmse(actual_shares, pred_shares) 
+print("RBF Model RMSE:")
+print(rmse_result)
+# observation -> when size is increased, the predicted values become more and more similar
+
+# GRNN Model
+pred_grnn <- function(x, nn){
+  xlst <- split(x, 1:nrow(x))
+  pred <- foreach(i = xlst, .combine = rbind) %dopar% {
+    data.frame(pred = guess(nn, as.matrix(i)), i, row.names = NULL)
+  }
+}
+
+library(grnn)
+library(doSNOW)
+library(doParallel)
+nn_df <- train_scaled_df
+grnn <- smooth(learn(nn_df, variable.column = ncol(nn_df)), sigma=0.2)
+pred <- pred_grnn(nn_df, grnn) # pick 10 rows
+
+pred_shares <-  pred * (max_share_val - min_share_val) + min_share_val
+actual_shares <- (nn_df$shares * (max_share_val - min_share_val)) + min_share_val
+rmse_result <- rmse(actual_shares, pred_shares) 
+print("GRNN Model RMSE:")
+print(rmse_result)
+
+# SEARCH FOR THE OPTIMAL VALUE OF SIGMA BY THE VALIDATION SAMPLE
+# cv <- foreach(s = seq(0.2, 1, 0.05), .combine = rbind) %dopar% {
+#   grnn <- smooth(learn(train_df, variable.column = ncol(train_df)), sigma = s)
+#   pred <- pred_grnn(set2[, -ncol(set2)], grnn)
+#   test.sse <- sum((set2[, ncol(set2)] - pred$pred)^2)
+#   data.frame(s, sse = test.sse)
+# }
+
+
 
 
 # library(neuralnet)
