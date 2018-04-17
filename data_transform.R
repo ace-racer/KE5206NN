1971cacf009e45cce61781436cb0087d28849533
Pier <madstrum@gmail.com>
2018-04-17 10:07:37 +0800

    first

---
+++ data_transform.R
@@ -0,0 +1,17 @@
+
+rm(list=ls())
+setwd('C:/Users/pierl/OneDrive/Documents/R_Projects/KE5206_Computational_Intelligence')
+train_df <- read.csv('./data/train_70.0.csv')
+
+# combine data_channel_is_? and weekday_is_? fields
+train_df$weekday <- paste(train_df$weekday_is_monday, train_df$weekday_is_tuesday, train_df$weekday_is_wednesday, train_df$weekday_is_thursday, 
+                 train_df$weekday_is_friday, train_df$weekday_is_saturday, train_df$weekday_is_sunday, sep='')
+
+train_df$data_channel_type <- paste(train_df$data_channel_is_bus, train_df$data_channel_is_entertainment, train_df$data_channel_is_lifestyle,
+                                    train_df$data_channel_is_socmed, train_df$data_channel_is_tech, train_df$data_channel_is_world, sep='')
+
+# remove data_channel_is_? and weekday_is_? fields
+train_df <- train_df[,-15:-20] 
+train_df <- train_df[,-27:-33]
+
+write.csv(train_df, file = "train_processed.csv")
