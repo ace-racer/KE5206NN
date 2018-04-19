97ce685a0273264a73ba843d696c08c36273f670
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-04-19 21:25:16 +0800

    Convert code to create complex CNN to script for GPU

---
+++ classification/CNNTrainingGPU.py
@@ -0,0 +1,124 @@
+#Import the required libraries
+import numpy as np
+import matplotlib.pyplot as plt
+np.random.seed(1338)
+
+from keras.datasets import fashion_mnist
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from keras.utils import np_utils
+from keras.optimizers import SGD
+
+# Set the number of EPOCHS
+nb_epoch = 75
+
+# Load the training and testing data
+(X_train, Y_train), (X_test, Y_test) = fashion_mnist.load_data()
+# Display purpose:
+X_train_orig = X_train
+X_test_orig = X_test
+
+from keras import backend as K
+img_rows, img_cols = 28, 28
+
+if K.image_data_format() == 'channels_first':
+    shape_ord = (1, img_rows, img_cols)
+else:  # channel_last
+    shape_ord = (img_rows, img_cols, 1)
+
+X_train = X_train.reshape((X_train.shape[0],) + shape_ord)
+X_test = X_test.reshape((X_test.shape[0],) + shape_ord)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+X_train /= 255
+X_test /= 255
+
+item_label_mapping = ["T-shirt/Top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle Boot"]
+
+print(X_train.shape, Y_train.shape, X_test.shape, Y_test.shape)
+
+# Converting the classes to its binary categorical form
+nb_classes = 10
+Y_train = np_utils.to_categorical(Y_train, nb_classes)
+Y_test = np_utils.to_categorical(Y_test, nb_classes)
+
+print(X_train.shape, Y_train.shape, X_test.shape, Y_test.shape)
+
+
+# -- Initializing the values for the convolution neural network
+
+batch_size = 64
+# number of convolutional filters to use
+nb_filters = 32
+# size of pooling area for max pooling
+nb_pool = 2
+# convolution kernel size
+nb_conv = 3
+
+# Vanilla SGD
+sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+
+
+
+
+def build_model():
+    """"""
+    model = Sequential()
+    model.add(Conv2D(nb_filters, (nb_conv, nb_conv),
+                     padding='valid',
+                     input_shape=shape_ord))
+    model.add(Activation('relu'))
+    model.add(Conv2D(nb_filters, (nb_conv, nb_conv)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+    model.summary()
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='sgd',
+                  metrics=['accuracy'])
+    hist = model.fit(X_train, Y_train, batch_size=batch_size,
+                     epochs=nb_epoch, verbose=1,
+                     validation_data=(X_test, Y_test))
+
+    # Evaluating the model on the test data
+    score, accuracy = model.evaluate(X_test, Y_test, verbose=0)
+    print('Test score:', score)
+    print('Test accuracy:', accuracy)
+    return hist, model
+
+
+# Train and test model in one shot
+hist, model = build_model()
+
+plt.figure()
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.legend(['Training', 'Validation'])
+
+plt.figure()
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.plot(hist.history['acc'])
+plt.plot(hist.history['val_acc'])
+plt.legend(['Training', 'Validation'], loc='lower right')
+
+#Evaluating the model on the test data
+score, accuracy = model.evaluate(X_test, Y_test, verbose=0)
+print('Test score:', score)
+print('Test accuracy:', accuracy)
+
+model.save('cnn_complex.h5')
